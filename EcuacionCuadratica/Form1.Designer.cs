d24aa54b67399213a1a6e3f73ef84ecf5291c7e7
Dina <dinamar582@hotmail.com>
2020-02-23 20:09:42 -0500

    aplicacion para calcular las raices de una ecuacion cuadratica

---
+++ EcuacionCuadratica/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace EcuacionCuadratica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textEcuacion = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.raiz1 = new System.Windows.Forms.Label();
+            this.raiz2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(146, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(296, 169);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label1.Location = new System.Drawing.Point(12, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese la Ecuacion cuadratica de la forma Ax2+Bx+C=R";
+            // 
+            // textEcuacion
+            // 
+            this.textEcuacion.Location = new System.Drawing.Point(360, 208);
+            this.textEcuacion.Name = "textEcuacion";
+            this.textEcuacion.Size = new System.Drawing.Size(227, 20);
+            this.textEcuacion.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.button1.Location = new System.Drawing.Point(211, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calcular las raices";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // raiz1
+            // 
+            this.raiz1.AutoSize = true;
+            this.raiz1.Location = new System.Drawing.Point(54, 292);
+            this.raiz1.Name = "raiz1";
+            this.raiz1.Size = new System.Drawing.Size(26, 13);
+            this.raiz1.TabIndex = 4;
+            this.raiz1.Text = "X1: ";
+            // 
+            // raiz2
+            // 
+            this.raiz2.AutoSize = true;
+            this.raiz2.Location = new System.Drawing.Point(328, 292);
+            this.raiz2.Name = "raiz2";
+            this.raiz2.Size = new System.Drawing.Size(26, 13);
+            this.raiz2.TabIndex = 5;
+            this.raiz2.Text = "X2: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 341);
+            this.Controls.Add(this.raiz2);
+            this.Controls.Add(this.raiz1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textEcuacion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textEcuacion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label raiz1;
+        private System.Windows.Forms.Label raiz2;
+    }
+}
+
---
+++ EcuacionCuadratica/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EcuacionCuadratica
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//manejo de secciones  
+			try
+			{
+				String sentence = textEcuacion.Text;
+				char[] charArr = sentence.ToCharArray(); //convierte un string a un  array de caracteres
+				String letra = "";
+				//recorre cada uno de los caracteres y encuentra la letra
+				foreach (char ch in charArr)
+				{
+					if (char.IsLetter(ch))
+					{
+						letra = "" + ch; break;
+					}
+				}
+				if (letra == "")
+				{
+					MessageBox.Show("Ha ocurrido un error , no se han ingresado las variables");
+					return;
+				}
+
+				int letra1 = sentence.IndexOf(letra + "2");     //Busco la ubicacion de x^2
+				String A = sentence.Substring(0, letra1);  //Recorto el valor de A
+
+				int ubicacionletra = letra1 + 2; //me ubico donde empieza B
+
+				String subSentence = sentence.Substring(ubicacionletra, (sentence.Length - ubicacionletra)); //recorto desde donde empieza B hasta el final
+				int letra2 = subSentence.IndexOf(letra); //busco la segunda letra en el substring
+				String B = sentence.Substring(ubicacionletra, letra2); //Recorto el valor de B
+
+				int ubicacionletra2 = letra2 + 1;
+				String subsubSentence = subSentence.Substring(ubicacionletra2, (subSentence.Length - ubicacionletra2)); //recorto desde donde empieza C hasta el final
+				int letra3 = subSentence.IndexOf("="); //busco el igual en el substring
+				String D = subSentence.Substring(ubicacionletra2, letra3 - ubicacionletra2); //Recorto el valor de D			
+
+				int ubicacionletra3 = letra3 + 1;
+				String R = subSentence.Substring(ubicacionletra3, (subSentence.Length - ubicacionletra3)); //recorto el valor de R
+
+				operacion(A, B, D, R);
+			}
+			catch (ArgumentException)
+			{
+				MessageBox.Show("Ha ocurrido un error , verifique  su funcion");
+			}
+
+		}
+
+		private void operacion(String A, String B, String D, String R)
+		{
+			//convertimos a numeros los valores   
+			double numA = double.Parse(A);
+			double numB = double.Parse(B);
+			double numD = double.Parse(D);
+			double numR = double.Parse(R);
+			double numC = numD - numR;
+			double x1;
+			double x2;
+			 //validamos que no pueda ingresar los numeros negativos en la raiz
+			if (Math.Sqrt(Math.Pow(numB, 2) - (4 * numA * numC)) > 0)
+			{
+				//calculamos las raices
+				x1 = (-numB + Math.Sqrt(numB * numB - 4 * numA * numC)) / (2 * numA);
+				x2 = (-numB - Math.Sqrt(numB * numB - 4 * numA * numC)) / (2 * numA);
+
+				raiz1.Text= "El valor de la raiz positiva es: " + x1;
+				raiz2.Text ="El valor de la raiz negativa es: " + x2;
+			}
+			else
+			{
+				MessageBox.Show("Error en la raíz, el contenido de la raiz no puede ser negativo");
+			}
+		}
+
+	}
+}
